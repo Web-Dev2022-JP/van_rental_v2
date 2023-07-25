b65f4791c9d9f3fe6c78b1bdd051897ccd2aa771
Ban <121280104+ivandnd@users.noreply.github.com>
2023-07-25 20:17:55 +0800

    Add message box and statistics

--- resources/views/dashboard/client/message-driver.blade.php
+++ resources/views/dashboard/client/message-driver.blade.php
@@ -47,6 +47,64 @@
     padding: 5px;
     border-radius: 90%;
 }
+
+.status {
+  top: 40px;
+  left: 55px;
+}
+
+.offline {
+  color: whitesmoke;
+  font-weight: 600;
+  background-color: gray;
+  border-radius: 5px;
+  padding: 5px;
+}
+
+.online {
+  color: whitesmoke;
+  font-weight: 600;
+  background-color: green;
+  border-radius: 5px;
+  padding: 5px;
+}
+
+.offline::after {
+  content: "• Offline";
+}
+
+.online::after {
+  content: "• Online";
+}
+
+.message-container {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 3;
+  flex-shrink: 0;
+  gap: 10px;
+}
+
+.message-container > .driver-message-container {
+  align-items: center;
+  background-color: gray;
+  padding: 10px;
+  color: whitesmoke;
+  border-radius: 5px 5px 5px 20px;
+}
+
+.message-container > .driver-message-container > p, .message-container > .user-message-container > p {
+  margin: 0;
+}
+
+.message-container > .user-message-container {
+  align-items: center;
+  background-color: skyblue;
+  text-align: right;
+  padding: 10px;
+  color: whitesmoke;
+  border-radius: 5px 20px 5px 5px;
+}
 </style>
 @endsection
 
@@ -54,9 +112,29 @@
 
 <div class="messageDriver-container row justify-content-center p-5">
   <form class="message-card" action="" method="POST">
-    <div class="driver-details d-flex align-items-center p-2 gap-2">
+    <div class="driver-details d-flex align-items-center p-2 gap-2 position-relative">
       <i class='bx bxs-user'></i>
       <div class="driver-name">JOHN DOE DEE NALIGO</div>
+      <div class="status">
+        <div class="user-status online"></div>
+      </div>
+    </div>
+    <div class="message-container">
+      <div class="driver-message-container">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates fugit, reprehenderit magni veniam unde accusamus nisi magnam possimus eveniet, quas consequuntur sequi itaque illum doloremque esse eum amet voluptas at.</p>
+      </div>
+      <div class="user-message-container">
+        <p>Meow</p>
+      </div>
+      <div class="user-message-container">
+        <p>Meow</p>
+      </div>
+      <div class="user-message-container">
+        <p>Meow</p>
+      </div>
+      <div class="driver-message-container">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates fugit, reprehenderit magni veniam unde accusamus nisi magnam possimus eveniet, quas consequuntur sequi itaque illum doloremque esse eum amet voluptas at.</p>
+      </div>
     </div>
     <input type="text" name="message" id="message" placeholder="INPUT MESSAGE" autocomplete="off">
     <button class="send-message bx bx-up-arrow-alt"></button>
